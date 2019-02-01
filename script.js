112fd38baef53d6ad687e9686e9356ada6976b95
Haocheng401 <44624482+Haocheng401@users.noreply.github.com>
2019-01-31 16:37:55 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+//Spell #1
+
+$(".button1").click(function(){
+    $("#img2").fadeOut();
+});
+
+//Spell #2
+
+$(".button2").click(function(){
+    $("#img3").slideUp();
+});
+
+//Spell #3
+
+$(".button3").click(function(){
+    $("#img4").hide();
+});
+
+$(".button4").click(function(){
+    $("#img2").fadeIn();
+});
+
+$(".button5").click(function(){
+    $("#img3").slideDown();
+});
+
+$(".button6").click(function(){
+    $("#img4").show();
+});
